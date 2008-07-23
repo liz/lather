641f4a3ef7bcdca6c1c19cc259f8951534981b8e
Liz <liz@latherrinserepeat.org>
2008-07-22 20:18:00 -0700

    catching up

--- search/views.py
+++ search/views.py
@@ -4,10 +4,8 @@ from meowr.models import Article
 
 def search(request):
 	query		= request.GET['q']
-	results		= Article.live.filter(body__icontains=query)
+	results		= Article.objects.filter(body__icontains=query, status=Article.LIVE_STATUS)
 	template 	= loader.get_template('search/search.html')
 	context		= Context({'query': query, 'results': results})
 	response	= template.render(context)
 	return HttpResponse(response)
-	
-	
