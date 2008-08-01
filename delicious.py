6c8351719744dcd9d7277db9159941f6a0e58f9e
Liz <liz@latherrinserepeat.org>
2008-08-01 12:38:07 -0400

    fixing up tweet and delicious

--- delicious.py
+++ delicious.py
@@ -5,8 +5,9 @@ from syncr.app.delicious import DeliciousSyncr
 from syncr.delicious import models
 import re, datetime, time, MySQLdb
 setup_environ(settings)
+from django.conf import settings
 
 from syncr.app.delicious import DeliciousSyncr
-d = DeliciousSyncr('tricky', 'callico')
+d = DeliciousSyncr(settings.DELICIOUS_USER, settings.DELICIOUS_PASS)
 
 d.syncRecent(count=6, tag='blog')
