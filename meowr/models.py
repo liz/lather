001cb08ed2d42ed8ef9b908c2ba4ee17a57fe3ac
Liz <liz@latherrinserepeat.org>
2008-08-05 16:11:27 -0400

    fixing up navigation

--- meowr/models.py
+++ meowr/models.py
@@ -129,3 +129,9 @@ class ArticleModerator(CommentModerator):
 	close_after			= 30
 
 moderator.register(Article, ArticleModerator)
+
+def get_previous_post(self):
+    return self.get_previous_by_pub_date(status__gte=1)
+
+def get_next_post(self):
+    return self.get_next_by_pub_date(status__gte=1)
