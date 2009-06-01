4b5ef70554b1ab6f0be011963437afacc0ce2dff
liz <liz@latherrinserepeat.org>
2009-05-31 21:21:35 -0400

    lil slower

--- static/spectrum/javascripts/spectrumd.js
+++ static/spectrum/javascripts/spectrumd.js
@@ -1,6 +1,6 @@
 $(function() {
 	if($('body#index').length > 0) {
-		setTimeout('animatedMenu()', 600);
+		setTimeout('animatedMenu()', 700);
 	} else {
 		$('#top-menu li a').css('opacity', 1);
 	}
